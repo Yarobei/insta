b16926b375ad805e7c0cd79bcf2c46f3eeb1d230
Dima <dima3911784@hotmail.com>
2019-10-24 08:59:30 +0300

    Second day

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,21 +3,13 @@ import User from "./User";
 
 export default class Post extends Component {
   render() {
+    const { name, altname, userphoto, src, alt, descr, min } = this.props;
     return (
       <div className="post">
-        <User
-          src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-          alt="man"
-          name="Scott"
-          min
-        />
-        <img src={this.props.src} alt={this.props.alt}></img>
-        <div className="post__name">some account</div>
-        <div className="post__descr">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, nihil!
-          Ab, harum eius fugiat recusandae sunt, tempora aut optio voluptas
-          omnis id officiis nulla dicta quas facilis commodi ad laboriosam.
-        </div>
+        <User src={userphoto} alt={altname} name={altname} min={min} />
+        <img src={src} alt={alt}></img>
+        <div className="post__name">{name}</div>
+        <div className="post__descr">{descr}</div>
       </div>
     );
   }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,63 @@
 import React, { Component } from "react";
+import InstaService from "../services/instaService";
 import Post from "./Post";
+import ErrorMessage from "./Error";
 
 export default class Posts extends Component {
+  InstaService = new InstaService();
+  state = {
+    posts: [],
+    error: false
+  };
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError);
+  }
+
+  onPostsLoaded = posts => {
+    this.setState({
+      posts,
+      error: false
+    });
+    console.log(this.state);
+  };
+
+  onError = () => {
+    this.setState({
+      error: true
+    });
+  };
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { name, altname, photo, src, alt, descr, id } = item;
+      return (
+        <Post
+          key={id}
+          src={src}
+          alt={alt}
+          name={name}
+          descr={descr}
+          userphoto={photo}
+          altname={altname}
+          min
+        />
+      );
+    });
+  }
+
   render() {
-    return (
-      <div className="left">
-        <Post
-          src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-          alt="first"
-        />
-        <Post
-          src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg"
-          alt="second"
-        />
-      </div>
-    );
+    const { posts, error } = this.state;
+    if (error) {
+      return <ErrorMessage />;
+    }
+    const items = this.renderItems(posts);
+    return <div className="left">{items}</div>;
   }
 }
